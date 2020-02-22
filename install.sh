e809c39359793ce16ca024777d1c8e075d07e116
dnhckt <hacketta.t@gmail.com>
2020-02-22 16:13:39 +0000

    fixed installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 mkdir /opt/ipdb/
-mv ./abusecheck.py /opt/ipdb/
-mv install.sh /opt/ipdb/
+cp ./abusecheck.py /opt/ipdb/abusecheck.py
+cp install.sh /opt/ipdb/install.sh
 
 echo "alias ipdb='python /opt/ipdb/abusecheck.py'" >> ~/.bashrc
 source ~/.bashrc
 
 read -p "Enter your API key:" apikey
-export $ABUSE_API_KEY=$apikey
+export ABUSE_API_KEY="$apikey"
