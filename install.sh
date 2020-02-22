af0d0a526c0b50fbc8aeddaa2f604bd96c075056
dnhckt <hacketta.t@gmail.com>
2020-02-22 16:18:04 +0000

    install script now runs source

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#! /bin/bash -i
 
 mkdir /opt/ipdb/
-cp ./abusecheck.py /opt/ipdb/abusecheck.py
-cp install.sh /opt/ipdb/install.sh
+#mv ./abusecheck.py /opt/ipdb/
+#mv install.sh /opt/ipdb/
 
 echo "alias ipdb='python /opt/ipdb/abusecheck.py'" >> ~/.bashrc
 source ~/.bashrc
