08a6e774820219bd8368b0f92665bdb3f38373c2
dnhckt <hacketta.t@gmail.com>
2020-05-19 10:21:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ cp ./abusecheck.py /opt/ipdb/
 #mv install.sh /opt/ipdb/
 
 echo "alias ipdb='python /opt/ipdb/abusecheck.py'" >> ~/.bashrc
-source ~/.bashrc
 
 read -p "Enter your API key:" apikey
 export ABUSE_API_KEY="$apikey"
+
+source ~/.bashrc
