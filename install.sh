8c0b5148d0e3af75eade7296f526fdab613f1563
dnhckt <hacketta.t@gmail.com>
2021-07-14 00:03:50 +0100

    move to /usr/local/bin instead of aliasing

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #! /bin/bash -i
 
-mkdir /opt/ipdb/
-cp ./abusecheck.py /opt/ipdb/
-#mv install.sh /opt/ipdb/
-
-echo "alias ipdb='python /opt/ipdb/abusecheck.py'" >> ~/.bashrc
+cp abusecheck.sh /usr/local/bin/abusecheck
+chmod +x /usr/local/bin/abusecheck
 
 read -p "Enter your API key:" apikey
 export ABUSE_API_KEY="$apikey"
-
-source ~/.bashrc
